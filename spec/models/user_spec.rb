24da930d277f87b4106d3abeba8e3b2a3437ce51
ceciliakemiac <ceciliakemiac2@gmail.com>
2019-02-01 14:15:27 -0300

    aspas duplas para simples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
   it { expect(user).to validate_presence_of(:email) }
   it { expect(user).to validate_uniqueness_of(:email).case_insensitive }
   it { expect(user).to validate_confirmation_of(:password) }
-  it { expect(user).to allow_value("nina@gmail.com").for(:email) }
+  it { expect(user).to allow_value('nina@gmail.com').for(:email) }
 
   # context "when name is blank" do
   #   before { user.name = "" }
