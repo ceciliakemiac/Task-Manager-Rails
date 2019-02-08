df3730014bddb046f3277434bde6e00197ed67c1
ceciliakemiac <ceciliakemiac2@gmail.com>
2019-02-08 12:01:00 -0300

    token do user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe User, type: :model do
   it { expect(user).to validate_uniqueness_of(:email).case_insensitive }
   it { expect(user).to validate_confirmation_of(:password) }
   it { expect(user).to allow_value('nina@gmail.com').for(:email) }
+  it { expect(user).to validate_uniqueness_of(:auth_token) }
 
   # context "when name is blank" do
   #   before { user.name = "" }
@@ -20,4 +21,11 @@ RSpec.describe User, type: :model do
   # it {is_expected.to respond_to(:password)}
   # it {expect(user).to respond_to(:password_confirmation)}
   # it {expect(user).to be_valid}
+
+  describe '#info' do
+    it 'returns email and created_at' do
+      user.save!
+      expect(user.info).to eq("#{user.email} - #{user.created_at}")
+    end
+  end
 end
