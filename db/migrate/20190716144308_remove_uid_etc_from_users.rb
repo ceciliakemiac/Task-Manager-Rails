c7ccefc39e14031d93a5bd64166b2afdda6431ad
ceciliakemiac <ceciliakemiac2@gmail.com>
2019-07-16 17:50:21 -0300

    delete migrates

--- db/migrate/20190716144308_remove_uid_etc_from_users.rb
+++
@@ -1,7 +0,0 @@
-class RemoveUidEtcFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :uid, :string
-    remove_column :users, :provider, :string
-    remove_column :users, :tokens, :text
-  end
-end
